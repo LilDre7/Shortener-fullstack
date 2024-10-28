ff898420e040e9fef271a8ff676ee660229a0c52
LilDre7 <alvaroaburto71@gmail.com>
2024-10-28 16:26:49 -0600

    :zap: Improve performace

--- app/api/urls/route.ts
+++ app/api/urls/route.ts
@@ -1,20 +1,29 @@
 import prisma from "@/lib/db";
 import { NextResponse } from "next/server";
 
-export const dynamic = "force-dynamic"
+export const dynamic = "force-dynamic";
 
 export async function GET() {
   try {
+    console.log("Conectando a la base de datos...");
+    
+    // Intentar realizar una consulta simple para probar la conexión
+    await prisma.$connect();
+    
     const urls = await prisma.url.findMany({
       orderBy: { createdAt: "desc" },
       take: 5,
     });
+    
+    console.log("URLs obtenidas:", urls); // Para ver los datos devueltos
     return NextResponse.json(urls);
   } catch (error) {
-    console.log("Error fetching URLs 💀 ", error);
+    console.error("Error fetching URLs 💀 ", error || error); // Detalles del error
     return NextResponse.json(
-      { error: "Internal Server Error 💀 " },
+      { error: error || "Internal Server Error 💀 " },
       { status: 500 }
     );
+  } finally {
+    await prisma.$disconnect(); // Asegúrate de desconectar
   }
 }
